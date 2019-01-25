0f3328886494b3e5046ce6305c9b264dac030808
AmirAmine1992 <b.a.amine1992@gmail.com>
2019-01-25 18:10:25 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -12,6 +12,8 @@
 									<ul>
 											<li><a href="event_list_passee.php">Events Passé</a></li>
 											<li><a href="event_list_future.php">Events Future</a></li>
+											<li><a href="event_list_future.php">Event du Mois</a></li>
+
 										</ul>
 									<li>
 										<a href="ideabox.php">Boîte à idée</a>
